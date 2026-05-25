b6fc6319196bf29a838463d717c232865e6dd8e0
CareOS Developer <developer@careos.local>
2026-05-25 14:06:22 +0500

    fix: cors allow all origins

--- server/server.js
+++ server/server.js
@@ -68,9 +68,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: function(origin, callback) {
+    callback(null, true)
+  },
   credentials: true
-}));
+}))
+app.options('*', cors())
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(compression());
